3ceba300cb0dab4c59c9418c04d86c84327b9cb8
alanecosta <alanecosta@gmail.com>
2024-06-07 17:31:12 -0300

    Métodos simples aplicados em Array

---
+++ MetodosJS.js
@@ -0,0 +1,21 @@
+const fruits = ['apple','banana','orange']
+console.log(fruits)
+
+fruits.pop()
+console.log(fruits)
+
+fruits.push('melon')
+console.log(fruits)
+
+fruits.shift()
+console.log(fruits)
+
+fruits.unshift()
+console.log(fruits)
+
+fruits.splice(2, 0, 'apple', 'orange')
+console.log(fruits)
+
+fruits.splice(0,1)
+console.log(fruits)
+
